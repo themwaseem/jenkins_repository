b989586f1a809e31fcb918dabd9c3b1cf85d7061
themwaseem <themwaseem@gmail.com>
2025-01-10 22:25:15 +0500

    Removing 4th test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,12 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
-    @Test
-  public void khud_fail_kia() {
-
-    assertTrue(true);
-  }
-
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
